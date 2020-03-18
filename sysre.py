330ecf6582b52617b8c33d079609f4e22978d819
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-18 19:40:01 -0400

    Create sysre.py

---
+++ sysre.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Note: python should be installed on your Windows machine if you would like to use sysre in the command prompt
+
+try:
+  import os
+  import sys
+  import subprocess
+  from colored import fg, bg, attr
+except ImportError as err:
+  print(f'Import Error: {}')
+  
+def main():
+  pass
+  
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    try:
+      print('Quiting program...')
+      sys.exit(1)
+    except SystemExit:
+      os._exit(1)
