fe460626dd08293c4c125f99e4a30db259b954d7
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-03-19 16:46:12 -0400

    updating repo

--- sysre.c
+++ sysre.c
@@ -1,34 +1,16 @@
-/*************************************//**
-* Author : Alexis Rodriguez
-* Date : 2020-03-18
-* Program : Sysre
-*****************************************/
+/***************************************************************************//**
+  @file         sysre.h
+  @author       Alexis Rodriguez
+  @date         Created Thursday, 19 March 2020
+  @brief        Sysre functionality
+*******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <dirent.h>
-
-
-//        Colors        //
-#define RED     "\x1b[91m"
-#define GREEN   "\x1b[92m"
-#define YELLOW  "\x1b[93m"
-#define BLUE    "\x1b[94m"
-#define MAGENTA "\x1b[95m"
-#define CYAN    "\x1b[36m"
-//    Original color    //
-#define RESET   "\x1b[0m" 
-
-
-#ifdef _WIN32
-    #define OPERATING_SYSTEM "Windows"
-    #include <Windows.h>
-#else
-    #define OPERATING_SYSTEM "Unix"
-    #include <unistd.h>
-#endif
+#include "sysre.h"
 
 
 FILE* exec_cmd(const char cmd[] );
---
+++ sysre.h
@@ -0,0 +1,30 @@
+/***************************************************************************//**
+  @file         sysre.h
+  @author       Alexis Rodriguez
+  @date         Created Thursday, 19 March 2020
+  @brief        Sysre declarations
+*******************************************************************************/
+
+#ifndef SYSRE_H
+#define SYSRE_H
+
+//        Colors        //
+#define RED     "\x1b[91m"
+#define GREEN   "\x1b[92m"
+#define YELLOW  "\x1b[93m"
+#define BLUE    "\x1b[94m"
+#define MAGENTA "\x1b[95m"
+#define CYAN    "\x1b[36m"
+//    Original color    //
+#define RESET   "\x1b[0m" 
+
+
+#ifdef _WIN32
+    #define OPERATING_SYSTEM "Windows"
+    #include <Windows.h>
+#else
+    #define OPERATING_SYSTEM "Unix"
+    #include <unistd.h>
+#endif
+
+#endif SYSRE_H // end sysrh_h
